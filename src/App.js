0e5a074b78cdc3cf4bbbbb7a2da726c60aa6337a
Caroline Pisano <pisanocaroline@gmail.com>
2019-12-03 20:50:16 -0500

    Added woofmama branding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './styles/styles.scss';
+// import Hero from './components/Hero';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <Header/>
+        {/* <Hero/> */}
+      </header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <nav>
+        <a href="https://www.himama.com/">
+          <img src="logomobile.png" alt="WoofMama logo" />
+          <h2>woof<span>mama</span></h2>
+        </a>
+      </nav>
+    );
+}
+
+export default Header
