ff213203f20f6509077f0b444e79ff9bcd4d5a42
caropi <pisanocaroline@gmail.com>
2019-12-04 14:57:37 -0500

    adding footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,158 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <div className="footer__column">
+                <h3 className="footer__column--title"></h3>
+                <ul className="footer__column--list">
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__column">
+                <h3 className="footer__column--title"></h3>
+                <ul className="footer__column--list">
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__column">
+                <h3 className="footer__column--title"></h3>
+                <ul className="footer__column--list">
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__column">
+                <h3 className="footer__column--title"></h3>
+                <ul className="footer__column--list">
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer__column">
+                <h3 className="footer__column--title"></h3>
+                <ul className="footer__column--list">
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                    <li className="footer__column--list-item">
+                        <a className="footer__column--list-item-link" href="">
+                            
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
     )
 }
 
