a05e6eb8bedda8d9df7870933a3ea345be0efe15
caropi <pisanocaroline@gmail.com>
2019-12-04 15:35:01 -0500

    Adding copyright to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import FooterColumns from './FooterColumns'
+import FooterColumns from './FooterColumns';
+import Copyright from './Copyright';
 const Footer = () => {
     return (
       <footer>
           <FooterColumns/>
+          <Copyright/>
       </footer>
     );
 }
