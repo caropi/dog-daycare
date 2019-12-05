2812a49297a9ccd53ace3e9d7432fc7e9d2b844a
Caroline Pisano <pisanocaroline@gmail.com>
2019-12-05 01:55:18 -0500

    Updates

---
+++ precache-manifest.1a6f0a28b44eb556c98aa7012568159c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f457c00f2a8a59a6dddb95873d4bc571",
+    "url": "/dog-daycare/index.html"
+  },
+  {
+    "revision": "9898b9f89d3404bcf879",
+    "url": "/dog-daycare/static/css/main.0fa4a640.chunk.css"
+  },
+  {
+    "revision": "b6300249f6d1f97f7c2e",
+    "url": "/dog-daycare/static/js/2.e2c8d448.chunk.js"
+  },
+  {
+    "revision": "9898b9f89d3404bcf879",
+    "url": "/dog-daycare/static/js/main.b65ad5df.chunk.js"
+  },
+  {
+    "revision": "7711acf513f48ae3650c",
+    "url": "/dog-daycare/static/js/runtime-main.2e257b0e.js"
+  }
+]);
