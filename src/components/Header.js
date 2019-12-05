2a0d46e09c5d0ceeb1219ed536ebf29b04a2ffff
Caroline Pisano <pisanocaroline@gmail.com>
2019-12-05 00:58:30 -0500

    :paw_prints: finished responsive on header navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,38 +2,47 @@ import React from 'react'
 import CommonButton from './CommonButton'
 const Header = () => {
     return (
-        <div className="nav">
-            <nav className="wrapper nav__container">
-                <a href="https://www.himama.com/" className="nav__logo--link">
-                    <img src="logomobile.png" alt="WoofMama logo" className="nav__img" />
-                    <h2 className="nav__logo--title">
-                        woof<span className="nav__logo--title--span">mama</span>
-                    </h2>
-                </a>
-            <div className="nav__list-container">
-                    <ul className='nav__list-container--list'>
-                        <li className="nav__list-container--list-item">
-                            <a href="https://www.himama.com/">
-                                Who Uses WoofMama
-                            </a>
-                        </li>
-                        <li className="nav__list-container--list-item">
-                            <a href="https://www.himama.com/">
-                                Features
-                            </a>
-                        </li>
-                        <li className="nav__list-container--list-item">
-                            <a href="https://www.himama.com/">
-                                Resources
-                            </a>
-                        </li>
-                    </ul>
-                    <CommonButton btnClass={'common-button'} text="Start Now"/>
-                    {/* Button class could change to logged-in onClick if hooked up to state */}
-                    <CommonButton btnClass={'common-button logged-out'} text="Log In"/>
-            </div>
-            </nav>
-        </div>
+      <div className="nav">
+        <nav className="wrapper nav__container">
+          <a href="https://www.himama.com/" className="nav__logo--link">
+            <img
+              src="logomobile.png"
+              alt="WoofMama logo"
+              className="nav__img"
+            />
+            <h2 className="nav__logo--title">
+              woof<span className="nav__logo--title--span">mama</span>
+            </h2>
+          </a>
+          <div className="nav__list-container">
+            <ul className="nav__list-container--list desktopOnly">
+              <li className="nav__list-container--list-item">
+                <a href="https://www.himama.com/">Who Uses WoofMama</a>
+              </li>
+              <li className="nav__list-container--list-item">
+                <a href="https://www.himama.com/">Features</a>
+              </li>
+              <li className="nav__list-container--list-item">
+                <a href="https://www.himama.com/">Resources</a>
+              </li>
+            </ul>
+            <CommonButton btnClass={"common-button"} text="Start Now" />
+            {/* Button class could change to logged-in onClick if hooked up to state */}
+            <CommonButton btnClass={"common-button logged-out"} text="Log In" />
+          </div>
+          <ul className="mobileOnly nav__list-container--list">
+            <li className="nav__list-container--list-item">
+              <a href="https://www.himama.com/">Who Uses WoofMama</a>
+            </li>
+            <li className="nav__list-container--list-item">
+              <a href="https://www.himama.com/">Features</a>
+            </li>
+            <li className="nav__list-container--list-item">
+              <a href="https://www.himama.com/">Resources</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
 }
 
