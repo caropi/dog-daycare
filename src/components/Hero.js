4b383edd8d1ff55d417786f12a189ae3ecc01cdd
Caroline Pisano <pisanocaroline@gmail.com>
2019-12-04 20:07:32 -0500

    almost done nav on desktop

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,15 +3,21 @@ import CommonButton from './CommonButton';
 const Hero = () => {
     return (
       <div className="hero wrapper">
-        <img src="undraw_good_doggy_4wfq.svg" alt="Woman walking dog" className="hero__img" />
+        <img
+          src="undraw_good_doggy_4wfq.svg"
+          alt="Woman walking dog"
+          className="hero__img"
+        />
         <div className="hero__copy-container">
-          <h1 className="hero__copy-container--title">Lorem ipsum dolor sit amet consectetur.</h1>
+          <h1 className="hero__copy-container--title">
+            Pupper puggo, you are doing me a frighten woofe.
+          </h1>
           <p className="hero__copy-container--text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed
-            perspiciatis reiciendis labore veniam! Corrupti voluptates
-            dignissimos consequuntur earum harum ducimus?
+            Doggo ipsum wow such tempt adorable doggo ur givin me a spook sub
+            woofer most angery pupper I have ever seen, lotsa pats very hand
+            that feed shibe snoot.
           </p>
-          <CommonButton btnClass='common-button' text="Start Now" />
+          <CommonButton btnClass="common-button " text="Start Now" />
         </div>
       </div>
     );
